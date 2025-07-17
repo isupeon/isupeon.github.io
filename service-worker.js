4d9a829a0872bd483c82c831088c662d543004a7
isupeon <ameenmomo05@gmail.com>
2025-07-17 14:10:20 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+const cacheName = 'ameen-ps4-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/exploit.html',
+  '/exploit.js',
+  '/goldhen.bin',
+  '/stage2_9.00.bin',
+  '/luffy.jpg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
